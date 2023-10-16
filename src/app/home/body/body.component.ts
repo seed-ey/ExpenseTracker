9e5b301cdeaa636854d6710c4a1804d4d8a09ec7
seed-ey <123967084+seed-ey@users.noreply.github.com>
2023-10-16 10:09:25 +0100

    Body component

--- src/app/home/body/body.component.ts
+++ src/app/home/body/body.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Chart, registerables } from 'chart.js';
+// import 'chartjs-plugin-datalabels';
+
+Chart.register(...registerables);
+
 
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
-export class BodyComponent {
+export class BodyComponent implements OnInit {
+
+  ngOnInit(): void {
+    // this.renderLineChart()
+   
+  }
+  // renderLineChart() {
+  //   const linechart = document.getElementById('lineChart') as HTMLCanvasElement;
+  //   new Chart(linechart, {
+  //     type: 'line',
+  //     data: {
+  //       labels: [],
+  //       datasets: [
+  //         {
+  //           label: 'Budget',
+  //           data: ['', '', '',],
+  //           backgroundColor: 'rgba(252, 165, 66, 0.2)',
+  //           borderColor: 'rgba(255, 165, 66, 1)',
+  //           tension: 0.4,
+  //           borderWidth: 3,
+  //         },
+  //       ]
+  //     },
+  //     options: {
+  //       plugins: {
+  //         legend: {
+  //           display: false,
+  //         },
+  //       },
+  //       layout: {
+  //         padding: {
+  //           left: 30,
+  //           right: 50,
+  //         },
+  //       },
+  //       aspectRatio: 1.7,
+  //       scales: {
+  //         x: {
+  //           grid: {
+  //             display: false, // Disable x-axis grid lines
+  //           },
+  //         },
+  //         y: {
+  //           beginAtZero: true,
+  //           grid: {
+  //             display: false, // Disable y-axis grid lines
+  //           },
+  //         },
+  //       }
+  //     }
+  //   });
+  // }
+  
 
 }
