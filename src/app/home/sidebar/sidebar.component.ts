9a5320b6b96ff377e84cf596c93f20c6d8da2fdd
seed-ey <123967084+seed-ey@users.noreply.github.com>
2023-10-17 11:07:23 +0100

    Added some styling to the sidebar and navbar

--- src/app/home/sidebar/sidebar.component.ts
+++ src/app/home/sidebar/sidebar.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  constructor(private renderer: Renderer2, private el: ElementRef) {
+  }
+  
+  items = [
+    { icon: 'grid_view', text: 'Dashboards', special: true },
+    { icon: 'Settings', text: 'Settings', special: false },
+    { icon: 'person', text: 'Workflow', special: false },
+    { icon: 'inventory', text: 'Products', special: false },
+    { icon: 'mail', text: 'Messages', special: false },
+    { icon: 'report', text: 'Report', special: false },
+    { icon: 'insights', text: 'Analytics', special: false }
+  ];
 
+  selectedItem: any = null;
+
+  selectItem(item: any) {
+    this.selectedItem = item;
+    localStorage.setItem('selectedItem', JSON.stringify(item));
+    this.items.forEach((i) => {
+      this.renderer.removeClass(this.el.nativeElement.querySelector(`.${i.icon}`), 'active');
+    });
+  
+    // Add "active" class to the selected item
+    this.renderer.addClass(this.el.nativeElement.querySelector(`.${item.icon}`), 'active');
+  }
+  ngOnInit() {
+    const storedSelectedItem = localStorage.getItem('selectedItem');
+    if (storedSelectedItem) {
+      this.selectedItem = JSON.parse(storedSelectedItem);
+    }
+  }
 }
